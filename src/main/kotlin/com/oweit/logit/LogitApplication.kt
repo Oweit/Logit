0ee038c74b0986f42a65ae62bc577afc4eb61753
Jakob Lund Johannessen <jakob-lj@hotmail.com>
2020-11-10 13:35:06 +0100

    init

---
+++ src/main/kotlin/com/oweit/logit/LogitApplication.kt
@@ -0,0 +1,11 @@
+package com.oweit.logit
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LogitApplication
+
+fun main(args: Array<String>) {
+	runApplication<LogitApplication>(*args)
+}
---
+++ src/test/kotlin/com/oweit/logit/LogitApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.oweit.logit
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LogitApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
